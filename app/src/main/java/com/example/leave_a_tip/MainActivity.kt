2822a1c75d485a13fc676e4e2a8c16efd2741366
Kai Naschinski <kai@naschinski.de>
2025-03-14 18:32:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/leave_a_tip/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.leave_a_tip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.leave_a_tip.ui.theme.LeaveatipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
